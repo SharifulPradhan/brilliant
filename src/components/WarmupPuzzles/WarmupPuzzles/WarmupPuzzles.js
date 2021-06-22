c26e3ac2680d684dbba903945f83821114c7d63d
Shariful Pradhan Hridoy <sharifulpradhanhridoy@Gmail.com>
2021-06-22 18:56:07 +0600

    quiz 2 updated

--- src/components/WarmupPuzzles/WarmupPuzzles/WarmupPuzzles.js
+++ src/components/WarmupPuzzles/WarmupPuzzles/WarmupPuzzles.js
@@ -9,12 +9,22 @@ import joseph from '../../../images/joseph.png'
 import kevin from '../../../images/kevin.png'
 import './WarmupPuzzles.css'
 import AnswerModal from '../AnswerModal/AnswerModal';
+import questionsData from '../../../data/questions.json'
+import quiz2 from '../../../images/lying-test.png'
+import quizDifficulty from '../../../images/quiz-difficulty.png'
 
 
 const WarmupPuzzles = () => {
-  const [answers, setanswers] = React.useState([]);
+  const [questions, setQuestions] = React.useState([]);
+  const [answers, setAnswers] = React.useState([]);
   const [modalShow, setModalShow] = React.useState(false);
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
   console.log(answers)
+
+  console.log(questions[currentQuestion]?.choices)
+  React.useEffect(() => {
+    setQuestions(questionsData);
+  }, [])
 
   const onanswerSubmit = () => {
     if (answers.length === 3) {
@@ -28,11 +38,24 @@ const WarmupPuzzles = () => {
         alert('Wrong Answer')
       }
     }
-    else{
+    if (answers === 'White') {
+      setModalShow(true)
+    }
+    else {
       alert('Wrong Answer')
     }
+
   }
 
+  const handleNextQuestion = () => {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestion(nextQuestion);
+  }
+
+  const changeHandler = (e) => {
+    const quiz2Anwser = e.target.value;
+    setAnswers(quiz2Anwser)
+  }
 
   return (
     <div>
@@ -43,50 +66,158 @@ const WarmupPuzzles = () => {
           <p className='nav-link text-secondary me-5'>Logic</p>
         </div>
 
-        {/* course quiz details */}
-        <div className="col-5 mt-5">
-          <div>
-            <p>Joseph, Kevin, and Nicholas are 3 brothers, and the following statements about them are all true:</p>
-            <ul>
-              <li><img src={joseph} alt="" width="50px" />Joseph is not the youngest.</li>
-              <li><img src={kevin} alt="" width="50px" />Kevin is the oldest.</li>
-              <li><img src={nicholas} alt="" width="50px" />Nicholas is not the oldest.</li>
-            </ul>
-            <p>Drag the characters into the dotted boxes</p>
-          </div>
-
-          <div>
-            <div className="flexbox">
-              <QuizBoard id="younger" className="board" onImageDrop={setanswers} />
-              <QuizBoard id="middle" className="board" onImageDrop={setanswers} />
-              <QuizBoard id="older" className="board" onImageDrop={setanswers} />
+        {/* course quiz1 details 
+        ===============================================================================
+        ===============================================================================
+        */}
+        {currentQuestion === 0 &&
+          <div className="col-5 mt-5">
+            <div>
+              <p>Joseph, Kevin, and Nicholas are 3 brothers, and the following statements about them are all true:</p>
+              <ul>
+                <li><img src={joseph} alt="" width="50px" />Joseph is not the youngest.</li>
+                <li><img src={kevin} alt="" width="50px" />Kevin is the oldest.</li>
+                <li><img src={nicholas} alt="" width="50px" />Nicholas is not the oldest.</li>
+              </ul>
+              <p>Drag the characters into the dotted boxes</p>
             </div>
 
-            <div className="flexbox">
-              <Board id="board-1" className="board">
-                <Cards id="joseph" className="cards" draggable="true">
-                  <img src={joseph} alt="" width="150px" draggable="false" />
-                </Cards>
-              </Board>
-              <Board id="board-2" className="board">
-                <Cards id="kevin" className="cards" draggable="true">
-                  <img src={kevin} alt="" width="150px" draggable="false" />
-                </Cards>
-              </Board>
-              <Board id="board-3" className="board">
-                <Cards id="nicholas" className="cards" draggable="true">
-                  <img src={nicholas} alt="" width="150px" draggable="false" />
-                </Cards>
-              </Board>
+
+            {/* course quiz options 
+            ===============================================================================
+            ===============================================================================
+            */}
+            <div>
+              <div className="flexbox">
+
+                {
+                  questions[0]?.questionID.map(quiz => <QuizBoard id={quiz} className="board" onImageDrop={setAnswers} />)
+                }
+              </div>
+
+              <div className="flexbox">
+                {
+                  questions[0]?.choices.map(choice => <Board id={choice.boardID} className="board">
+                    <Cards id={choice.cardsID} className="cards" draggable="true">
+                      <img src={choice.image} alt="" width="150px" draggable="false" />
+                    </Cards>
+                  </Board>)
+                }
+              </div>
+              <button onClick={onanswerSubmit} className="btn-blue">Submit answer</button>
             </div>
-            <button onClick={onanswerSubmit} className="btn-blue">Submit answer</button>
           </div>
-        </div>
+        }
+
+        {/* course quiz2 details
+        ===============================================================================
+        ===============================================================================
+        */}
+        {currentQuestion === 1 &&
+          <div className="col-4 mt-5">
+            <div className="d-flex flex-column align-items-center">
+              <div>
+                <img src={quiz2} alt="" width="450px" />
+              </div>
+              <div>
+                <p>One of the women above is named Kaylee and the other is named Inara. They each make a statement about who they are as shown.</p>
+                <p>We know that <strong>at least one of them is lying.</strong> What color is Inara's dress?</p>
+              </div>
+            </div>
+          </div>
+        }
+
+        {/* course quiz difficulties details
+        ===============================================================================
+        ===============================================================================
+        */}
+        {currentQuestion === 2 &&
+          <div className="col-4 mt-5">
+            <div className="d-flex flex-column align-items-center">
+              <div>
+                <img src={quizDifficulty} alt="" width="450px" />
+              </div>
+              <div>
+                <p>The next three problems gradually increase in difficulty, and all of them are more challenging than the warm-ups you just solved.</p>
+                <p>It's worth the effort. The most effective learning experiences are often those times when you get a problem wrong, and then challenge yourself to read, understand, and learn from the solution.</p>
+              </div>
+            </div>
+          </div>
+        }
+
+        {/* course quiz3 details 
+        ===============================================================================
+        ===============================================================================
+        */}
+        {currentQuestion === 3 &&
+          <div className="col-5 mt-5">
+            <div>
+              <p>Joseph, Kevin, and Nicholas are 3 brothers, and the following statements about them are all true:</p>
+              <ul>
+                <li><img src={joseph} alt="" width="50px" />Joseph is not the youngest.</li>
+                <li><img src={kevin} alt="" width="50px" />Kevin is the oldest.</li>
+                <li><img src={nicholas} alt="" width="50px" />Nicholas is not the oldest.</li>
+              </ul>
+              <p>Drag the characters into the dotted boxes</p>
+            </div>
+
+
+            {/* course quiz options 
+            ===============================================================================
+            ===============================================================================
+            */}
+            <div>
+              <div className="flexbox">
+
+                {
+                  questions[0]?.questionID.map(quiz => <QuizBoard id={quiz} className="board" onImageDrop={setAnswers} />)
+                }
+              </div>
+
+              <div className="flexbox">
+                {
+                  questions[0]?.choices.map(choice => <Board id={choice.boardID} className="board">
+                    <Cards id={choice.cardsID} className="cards" draggable="true">
+                      <img src={choice.image} alt="" width="150px" draggable="false" />
+                    </Cards>
+                  </Board>)
+                }
+              </div>
+              <button onClick={onanswerSubmit} className="btn-blue">Submit answer</button>
+            </div>
+          </div>
+        }
+
 
         <div className="col-4 mt-5">
-          <p>Lesson 1 </p>
-          <h4>WARMUP PUZZLES</h4>
-          <button className="btn-blue">Next Question</button>
+          <div>
+            <p>Lesson 1 </p>
+            <h4>WARMUP PUZZLES</h4>
+          </div>
+          {currentQuestion === 1 &&
+            <div style={{ fontSize: '18px', fontWeight: 'bold' }}>
+              <div class="form-check p-2">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={changeHandler} value="Black" />
+                <label class="form-check-label" for="flexRadioDefault1">
+                  Black
+                </label>
+              </div>
+              <div class="form-check p-2">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={changeHandler} value="White" />
+                <label class="form-check-label" for="flexRadioDefault2">
+                  White
+                </label>
+              </div>
+              <div class="form-check p-2">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={changeHandler} value="There isn't enough information to be certain" />
+                <label class="form-check-label" for="flexRadioDefault2">
+                  There isn't enough information to be certain
+                </label>
+                <button onClick={onanswerSubmit} className="btn-blue bg-dark w-50">Submit answer</button>
+              </div>
+            </div>
+          }
+          <button className="btn-blue" onClick={handleNextQuestion}>Next Question</button>
           <AnswerModal
             show={modalShow}
             onHide={() => setModalShow(false)}
